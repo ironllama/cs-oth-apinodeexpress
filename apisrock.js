9d6779db6a65eb34af67aa4e1dd8973a508d5d65
Alex <alexoh@wcoding.com>
2023-02-16 00:00:13 +0900

    Updated during event.

--- apisrock.js
+++ apisrock.js
@@ -1,19 +1,32 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
+const port = 8888;
 
-const port = 8080;
-const db = fs.openSync('apisrock.db', 'a+');
+const petList = require('./pets.json').pets; // Since there's only one property, anyway.
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('Yo, dude!');
+
+app.get('/', function (req, res) {
+    // res.send('<h1>Yo, this is a great response.</h1>');
+    res.send({
+        "pets": petList.map(pet => ({
+            "name": pet.name
+        }))
+    });
 });
 
-app.post('/chat', (req, res) => {
-    console.log(req);
-    res.send('NADA');
+app.get('/pet/:id', (req, res) => {
+    let id = req.params.id;
+    if (id && id < petList.length) {
+        res.send(petList[id]);
+    } else {
+        res.status(404).send({
+            "error": "Pet not found."
+        });
+    }
 });
 
-app.listen(port, () => console.log('Up on ' + port));
+app.listen(port, function () {
+    console.log("Server is now up on " + port);
+});
